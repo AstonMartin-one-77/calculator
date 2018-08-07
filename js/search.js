f473602f3f131ea5fd9ace7f25bd83ff97a643b3
Georgy Zagoruiko <AstonMartin-one-77@outlook.com>
2018-08-07 15:31:28 +0300

    Search-field is making to DB and input for users

---
+++ js/search.js
@@ -0,0 +1,91 @@
+﻿'use strict';
+
+var baseCityList = new Array();
+var isReady = false;
+
+function requestBaseCityList(userString) {
+    $.ajax({
+        type: "post",
+        url: "php/search.php",
+        data: {
+            "baseCityString": userString
+        },
+        response: "text",
+        success: function(list) {
+            /* Обновляем список городов. */
+            setBaseCityList(list.cities);
+            var cities = getBaseCityList($("input#baseCity").val());
+            cities.forEach(function(item, index, array) {
+                array[index] = "\n<li><span class='city'>" + item + "</span></li>";
+            });
+            $("ul#baseCityResult").html(cities).fadeIn();
+        },
+        error: function(xhr, status, error) {
+            status.text();
+        },
+        dataType: "json"
+    });
+}
+
+function setBaseCityList(list) {
+    baseCityList = list;
+    isReady = true;
+}
+
+function getBaseCityList(userString) {
+    var result = ["Нет результатов"];
+    if (isReady) {
+        /* Формируем массив на основе совпадения с строкой пользователя. */
+        var baseList = $.grep(baseCityList, function(element, index) {
+            if (element.toLowerCase().indexOf(userString.toLowerCase()) === 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+        if (baseList.length > 0) {
+            result = baseList;
+        }
+    }
+    return result;
+}
+
+$(function() {
+    // ПУНКТ ОТПРАВЛЕНИЯ:
+    // 1. Отправка запроса:
+    $("input#baseCity").bind("change keyup input click", function() {
+        if (($(this).val().length % 2) === 0) {
+            var cities = getBaseCityList($("input#baseCity").val());
+            cities.forEach(function(item, index, array) {
+                array[index] = "\n<li><span class='city'>" + item + "</span></li>";
+            });
+            $("ul#baseCityResult").html(cities).fadeIn();
+            /* Если мало городов в списке - обновляем: */
+            if (cities.length < 10) {
+                // Запрос данных от сервера.
+                requestBaseCityList($(this).val());
+            }
+        }
+        else {
+            requestBaseCityList($(this).val());
+        }
+    });
+    // 3. При выборе результата поиска - спрятать список и занести результат в поле ввода:
+    $("ul#baseCityResult").on("click", "li", function() {
+        $("input#baseCity").val($(this).text());
+        $("ul#baseCityResult").fadeOut();
+    });
+    // 4. Спрятать список, если клик вне поля:
+    $(document).mouseup(function(event) {
+        var input = $("input#baseCity");
+        var list = $("ul#baseCityResult");
+
+        if (!input.is(event.target) && !list.is(event.target) && 
+            (input.has(event.target).length === 0) && 
+            (input.has(event.target).length === 0))
+        {
+            $("ul#baseCityResult").fadeOut();
+        }
+    });
+});
--- php/search.php
+++ php/search.php
@@ -1,16 +1,71 @@
 <?php 
 
-    $cityWrap = array("<span class='city'></span>");
-    $areaWrap = "<span class='cityArea'></span>";
-    $cities = array("Санкт-Петербург", "Москва", "Красноярск");
-    $aries = array("Ленинградская обл.", "Московская обл.", "Красноярский край");
-    
-    if (!empty($_POST["baseCityStr"])) {
-        $userString = $_POST["baseCityStr"];
+    class calculatorDB {
+
+        const DB_HOST = "calculator.ru";
+        const DB_NAME = "calculator";
+        const DB_USER = "calculatorUser";
+        const DB_PASSWORD = "calculator.ru";
+        private $sqlDB = null;
+        private $result = null;
         
-        for ($count = 0; $count < count($cities); ++$count) {
-            echo "\n<li><span class='city'>$cities[$count]</span> <span class='cityArea'>($aries[$count])</span></li>";
+        function __construct() {
+            $this->sqlDB = new mysqli(self::DB_HOST, self::DB_USER, self::DB_PASSWORD, self::DB_NAME);
+            if (!$this->sqlDB->query("SET NAMES 'utf8'")) {
+                $this->result["success"] = false;
+                $this->result["connect"] = "Could not connect to DB";
+            }
+            else {
+                $this->result["success"] = true;
+                $this->result["connect"] = "Success connect to DB";
+            }
         }
+      
+        function getBaseCityList($userString) {
+            $pattern = "/([А-Яа-я]+[\-[А-Яа-я]+]?|)/";
+            if ((true === $this->result["success"]) && (true === is_string($userString)) && 
+                /** Проверяем на паттерн, предварительно удалив все лишние символы из запроса. */
+                (1 === preg_match($pattern, $this->sqlDB->real_escape_string(trim(strip_tags($userString))), $matches))) {
+                /** Если найдено совпадение в запросе по паттерну, копируем результат. */
+                $userString = $matches[0];
+                /** Поиск в БД на совпадение по опорным городам. */
+                $data = $this->sqlDB->query("SELECT DISTINCT Base_City AS all_city FROM cities WHERE Base_City LIKE '$userString%' UNION SELECT DISTINCT City AS all_city FROM cities WHERE City LIKE '$userString%'");
+                $this->result["error"] = $this->sqlDB->error;
+                /** Проверяем результат. */
+                if (false === $data) {
+                    $this->result["success"] = false;
+                    $this->result["getBaseCityList"] = "Error";
+                }
+                else {
+                    $this->result["getBaseCityList"] = "success";
+                    $baseCities = array($data->num_rows);
+                    for ($i = 0; $i < $data->num_rows; ++$i) {
+                        $tmp = $data->fetch_row();
+                        $baseCities[$i] = $tmp[0];
+                    }
+                    $this->result["cities"] = $baseCities;
+                }
+            }
+        }
+      
+        function isSuccess() { return $this->result["success"]; }
+      
+        function getResult() { return $this->result; }
+        
     }
+    
+    $DB = new calculatorDB();
+    $DB->getBaseCityList($_POST["baseCityString"]);
+    
+    echo json_encode($DB->getResult());
+    
+    /*if (isset($_POST["baseCityString"])) {
+        $result = array();
+        for ($i = 0; $i < count($cities); ++$i) {
+            array_push($result, "$cities[$i] <span class='cityArea'>$aries[$i]</span>");
+        }
+      
+        echo json_encode($result);
+    }*/
 
 ?>
