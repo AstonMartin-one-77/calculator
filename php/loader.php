67144f8928c9f0c4b094bdb742fadf2a7b42791b
Georgy Zagoruiko <AstonMartin-one-77@outlook.com>
2018-09-22 19:23:15 +0300

    Make excel reader for the table of cities. You should checks functions in class ExcelFile and create class for load in DB

--- php/loader.php
+++ php/loader.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
     require '../vendor/autoload.php';
 	// Класс, непосредственно читающий файл
 	use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
@@ -32,6 +32,160 @@
 		}
     }
 
+    class ExcelFile {
+
+        private $_path = null;
+        private $_numEntries = 0;
+        private $_baseCity = null;
+        private $_list = null;
+        private $_startRow = 1;
+        private $_stepRows = 1000;
+        private $_result = null;
+        
+        public function __construct($path) {
+            $this->_path = $path;
+            // Создаем объект чтения таблицы.
+            $reader = new Xlsx();
+            // Требуется только чтение. Форматирование и остальные нюансы не нужны.
+            $reader->setReadDataOnly(true);
+            // Настраиваем фильтр для чтения части файла.
+            $filter = new LoadFilter($this->_startRow, $this->_stepRows, range('A', 'G'));
+            $reader->setReadFilter($filter);
+            // Загружаем файл.
+            $spreadsheet = $reader->load($this->_path);
+            // Достаем объект Cells, имеющий доступ к содержимому ячеек
+            $cells = $spreadsheet->getActiveSheet()->getCellCollection();
+            if (("Направление" != $cells->get('A'.'2')->getValue()) || 
+                ("Населенный пункт" != $cells->get('B'.'2')->getValue()) || 
+                ("СТАНДАРТ" != $cells->get('C'.'2')->getValue()) || 
+                ("ЭКСПРЕСС" != $cells->get('D'.'2')->getValue()) || 
+                ("СУПЕРЭКСПРЕСС" != $cells->get('E'.'2')->getValue()) || 
+                ("Зона доставки" != $cells->get('F'.'2')->getValue()) || 
+                ("Коэффициент" != $cells->get('G'.'2')->getValue()))
+            {
+                $this->_result = false;
+            }
+            else {
+                $this->_list = $this->readCells($cells, 3, $this->_startRow + $this->_stepRows);
+                if ((null != $this->_list) && (0 < count($this->_list))) {
+                    $this->_startRow = $this->_startRow + $this->_stepRows;
+                    $this->_numEntries += count($this->_list);
+                    $this->_result = true;
+                } else {
+                    $this->_result = false;
+                }
+            }
+        }
+        
+        public function readNextPart() {
+            if (true == $this->_result) {
+                // Создаем объект чтения таблицы.
+                $reader = new Xlsx();
+                // Требуется только чтение. Форматирование и остальные нюансы не нужны.
+                $reader->setReadDataOnly(true);
+                // Настраиваем фильтр для чтения части файла.
+                $filter = new LoadFilter($this->_startRow, $this->_stepRows, range('A', 'G'));
+                $reader->setReadFilter($filter);
+                // Загружаем файл.
+                $spreadsheet = $reader->load($this->_path);
+                // Достаем объект Cells, имеющий доступ к содержимому ячеек
+                $cells = $spreadsheet->getActiveSheet()->getCellCollection();
+                // Читаем таблицу и формируем список:
+                $this->_list = $this->readCells($cells, $this->_startRow, $this->_startRow + $this->_stepRows);
+                if ((null != $this->_list) && (0 < count($this->_list))) {
+                    $this->_startRow = $this->_startRow + $this->_stepRows;
+                    $this->_numEntries += count($this->_list);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        
+        public function getCurPart() {
+            return $this->_list;
+        }
+        
+        public function getBaseCity() { return $this->_baseCity; }
+        
+        public function getResult() { return $this->_result; }
+        
+        private function readCells($cells, $startRow, $endRow) {
+            $tmpList = null;
+            for ($row = $startRow; $row < $endRow; ++$row) {
+                $entry = null;
+                // Читаем край/область и город доставки:
+                $area = $this->getArea($cells->get('A'.$row)->getValue());
+                $city = $this->getCity($cells->get('B'.$row)->getValue());
+                if ((null != $city) && (null != $area)) {
+                    $entry["city"] = "$city ($area)";
+                } else {
+                    break;
+                }
+                $standart = $this->getLimit($cells->get('C'.$row)->getValue());
+                if (null != $standart) {
+                    $entry["mode"]["standart"] = $standart;
+                } else {
+                    break;
+                }
+                $express = $this->getLimit($cells->get('D'.$row)->getValue());
+                if (null != $express) {
+                    $entry["mode"]["express"] = $express;
+                }
+                $spExpress = $this->getLimit($cells->get('E'.$row)->getValue());
+                if (null != $spExpress) {
+                    $entry["mode"]["superexpress"] = $spExpress;
+                }
+                $zone = $this->getZone($cells->get('E'.$row)->getValue());
+                if (null != $zone) {
+                    $entry["zone"] = $zone;
+                } else {
+                    break;
+                }
+                $coeff = $this->getCoeff($cells->get('E'.$row)->getValue());
+                if (null != $coeff) {
+                    $entry["coeff"] = $coeff;
+                } else {
+                    break;
+                }
+                $tmpList[$row] = $entry;
+            }
+            return $tmpList;
+        }
+        
+        private function getCity($city) {
+            $pattern = "/(([а-я]*)(-[а-я]+)?(-[а-я]+)?)/ui";
+            // TODO: Добавить защиту от взлома БД.
+            return $city;
+        }
+        
+        private function getArea($area) {
+            // TODO: Добавить защиту от взлома БД.
+            return $area;
+        }
+        
+        private function getLimit($limit) {
+            // TODO: Добавить проверку на формат.
+            if (("0-0" != $limit) && ("" != $limit)) {
+                return $limit;
+            } else {
+                return null;
+            }
+        }
+        
+        private function getZone($zone) {
+            // TODO:
+            return (integer) $zone;
+        }
+        private function getCoeff($coeff) {
+            // TODO:
+            return (float) $coeff;
+        }
+    }
+
     class DBLoader {
         const DB_HOST = "calculator.ru";
         const DB_NAME = "calculator";
@@ -63,6 +217,8 @@
                 }
             }
         }
+      
+        
         
         public function getResult() { return $this->result; }
     }
@@ -71,9 +227,13 @@
         $DB = new DBLoader();
         $result = $DB->getResult();
         $result["request"] = "correct";
+        $excelFile = new ExcelFile($_FILES[0]["tmp_name"]);
+        $result["baseCity"] = $excelFile->getBaseCity();
+        $result["excelFormat"] = $excelFile->getResult();
+        //$result["FILES"] = $_FILES;
         echo json_encode($result);
     }
-    /*if ( 0 < $_FILES["file"]["error"] ) {
+    /*if ( 0 < $_FILES;["file"]["error"] ) {
         echo 'Error: ' . $_FILES["file"]["error"] . "<br>";
     }
     else {
